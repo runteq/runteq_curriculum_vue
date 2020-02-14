8ceca1e98de5f4b7e44b61c969cf42aa020f54f9
Makoto Otsuka <mametra2580@gmail.com>
2020-02-14 14:46:05 +0900

    [Add]トップページのマークアップ実装

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -8,6 +8,8 @@
 import Vue from 'vue'
 import App from '../app.vue'
 
+import 'bootstrap/dist/css/bootstrap.css'
+
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
     render: h => h(App)
