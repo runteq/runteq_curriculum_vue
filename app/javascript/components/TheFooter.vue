47c1c3d2e17743cefa9e80ad75e5c5fc0e8b3ccb
Makoto Otsuka <mametra2580@gmail.com>
2020-02-18 18:51:08 +0900

    [add]Header,Footerコンポーネントの追加

---
+++ app/javascript/components/TheFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer class="text-center">
+    <small>Copyright &copy; 2020. RUNTEQ</small>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "TheHeader"
+}
+</script>
+
+<style scoped>
+
+</style>
