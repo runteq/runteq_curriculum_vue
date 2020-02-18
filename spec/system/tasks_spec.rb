41c98b1bacbe61c4652ed1ace68e26f033f96973
Makoto Otsuka <mametra2580@gmail.com>
2020-02-18 18:13:36 +0900

    [add]タスクページへの画面遷移に関するspecを追加

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,7 +1,24 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理', type: :system do
-  it 'トップページが表示されている' do
+  it 'トップページにはじめるボタンが表示されている' do
     visit root_path
-    expect(page).to have_content('はじめる'), 'はじめるボタンがトップページに表示されていません'
+    expect(page).to have_content('はじめる'), 'トップページにはじめるボタンが表示されていません'
+  end
+
+  it 'タスクページに戻るボタンが表示されている' do
+    visit '/tasks'
+    expect(page).to have_content('戻る'), 'タスクページに戻るボタンが表示されていません'
+  end
+
+  it 'トップページからタスクページに遷移できる' do
+    visit root_path
+    click_link 'はじめる'
+    expect(page).to have_content('戻る'), 'タスクページに遷移できていません'
+  end
+
+  it 'タスクページからトップページに遷移できる' do
+    visit '/tasks'
+    click_link '戻る'
+    expect(page).to have_content('はじめる'), 'トップページに遷移できていません'
   end
 end
