9f77eeebeb98897d57174b80a4349c16b90eeca9
Makoto Otsuka <mametra2580@gmail.com>
2020-02-18 11:38:18 +0900

    [add]タスクページの追加

---
+++ app/javascript/pages/TaskList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="d-flex flex-column min-vh-100">
+    <header class="mb-auto">
+      <nav class="navbar navbar-dark bg-dark">
+        <span class="navbar-brand mb-0 h1">{{ title }}</span>
+      </nav>
+    </header>
+    <div class="text-center">
+      <h3>タスクリスト</h3>
+        <router-link to="/">
+          <button type="button" class="btn btn-dark mt-5">戻る</button>
+        </router-link>
+    </div>
+    <footer class="mt-auto text-center">
+      <small>Copyright &copy; 2020. RUNTEQ</small>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TaskList",
+  data() {
+    return {
+      title: "タスク管理アプリ"
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
