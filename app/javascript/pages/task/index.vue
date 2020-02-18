3bca7982211b216931ed28b996194610c0d6fb65
Makoto Otsuka <mametra2580@gmail.com>
2020-02-18 12:34:07 +0900

    [update]名前付きルートの使用

--- app/javascript/pages/task/index.vue
+++ app/javascript/pages/task/index.vue
@@ -7,9 +7,7 @@
     </header>
     <div class="text-center">
       <h3>タスクリスト</h3>
-        <router-link to="/">
-          <button type="button" class="btn btn-dark mt-5">戻る</button>
-        </router-link>
+      <router-link :to="{ name: 'TopIndex' }" class="btn btn-dark mt-5">戻る</router-link>
     </div>
     <footer class="mt-auto text-center">
       <small>Copyright &copy; 2020. RUNTEQ</small>
