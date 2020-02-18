e3e11d830dd5332a81693943a880f292ac49f37f
Makoto Otsuka <mametra2580@gmail.com>
2020-02-18 15:49:30 +0900

    [update]タスクページにTODOリストを追加

--- app/javascript/pages/task/index.vue
+++ app/javascript/pages/task/index.vue
@@ -5,8 +5,15 @@
         <span class="navbar-brand mb-0 h1">{{ title }}</span>
       </nav>
     </header>
+    <div class="d-flex">
+      <div class="col-4 bg-light rounded shadow m-3 p-3">
+        <div class="h4">TODO</div>
+        <div v-for="task in tasks" :key="task.id" class="bg-white border shadow-sm rounded my-2 p-4">
+          <span>{{ task.title }}</span>
+        </div>
+      </div>
+    </div>
     <div class="text-center">
-      <h3>タスクリスト</h3>
       <router-link :to="{ name: 'TopIndex' }" class="btn btn-dark mt-5">戻る</router-link>
     </div>
     <footer class="mt-auto text-center">
@@ -20,7 +27,25 @@ export default {
   name: "TaskIndex",
   data() {
     return {
-      title: "タスク管理アプリ"
+      title: "タスク管理アプリ",
+      tasks: [
+        {
+          id: 1,
+          title: "スーパに買い物に行く"
+        },
+        {
+          id: 2,
+          title: "子供の迎えに行く"
+        },
+        {
+          id: 3,
+          title: "新聞を解約する"
+        },
+        {
+          id: 4,
+          title: "ゴミ出しをする"
+        },
+      ]
     }
   }
 }
