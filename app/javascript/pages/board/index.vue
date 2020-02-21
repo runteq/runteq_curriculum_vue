bf2df168f8ce4b5f5db3be2d4f893796d1ab16d0
Makoto Otsuka <mametra2580@gmail.com>
2020-02-21 15:43:38 +0900

    [update]タスクページのpathをtasksからboardsに変更

--- app/javascript/pages/board/index.vue
+++ app/javascript/pages/board/index.vue
@@ -24,7 +24,7 @@
 
 <script>
 export default {
-  name: "TaskIndex",
+  name: "BoardIndex",
   data() {
     return {
       title: "タスク管理アプリ",
--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -6,18 +6,18 @@ RSpec.describe 'タスク管理', type: :system do
   end
 
   it 'タスクページに戻るボタンが表示されている' do
-    visit '/tasks'
+    visit '/boards'
     expect(page).to have_content('戻る'), 'タスクページに戻るボタンが表示されていません'
   end
 
   it 'トップページからタスクページに遷移できる' do
     visit root_path
     click_link 'はじめる'
-    expect(current_path).to eq('/tasks'), 'タスクページに遷移できていません'
+    expect(current_path).to eq('/boards'), 'タスクページに遷移できていません'
   end
 
   it 'タスクページからトップページに遷移できる' do
-    visit '/tasks'
+    visit '/boards'
     click_link '戻る'
     expect(current_path).to eq('/'), 'トップページに遷移できていません'
   end
