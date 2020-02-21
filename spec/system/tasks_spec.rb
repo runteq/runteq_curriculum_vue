e1b82f42db959129dfee1c3e45fc6456cd5527f6
Makoto Otsuka <mametra2580@gmail.com>
2020-02-21 15:52:02 +0900

    [update]spec内のタスクをボードに変更

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -5,18 +5,18 @@ RSpec.describe 'タスク管理', type: :system do
     expect(page).to have_content('はじめる'), 'トップページにはじめるボタンが表示されていません'
   end
 
-  it 'タスクページに戻るボタンが表示されている' do
+  it 'ボードページに戻るボタンが表示されている' do
     visit '/boards'
-    expect(page).to have_content('戻る'), 'タスクページに戻るボタンが表示されていません'
+    expect(page).to have_content('戻る'), 'ボードページに戻るボタンが表示されていません'
   end
 
-  it 'トップページからタスクページに遷移できる' do
+  it 'トップページからボードページに遷移できる' do
     visit root_path
     click_link 'はじめる'
-    expect(current_path).to eq('/boards'), 'タスクページに遷移できていません'
+    expect(current_path).to eq('/boards'), 'ボードページに遷移できていません'
   end
 
-  it 'タスクページからトップページに遷移できる' do
+  it 'ボードページからトップページに遷移できる' do
     visit '/boards'
     click_link '戻る'
     expect(current_path).to eq('/'), 'トップページに遷移できていません'
