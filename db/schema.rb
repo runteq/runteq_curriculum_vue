0d7a90ded0cbbea27edd6845f4419b06a5333829
Makoto Otsuka <mametra2580@gmail.com>
2020-02-28 17:50:39 +0900

    [add]Tasksテーブルにdescriptionカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_27_055231) do
+ActiveRecord::Schema.define(version: 2020_02_28_084909) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title", null: false
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
