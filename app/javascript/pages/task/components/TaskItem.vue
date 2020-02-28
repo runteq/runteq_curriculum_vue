95db5d652bf1f3718117d142316da4f47657dc9e
Makoto Otsuka <mametra2580@gmail.com>
2020-02-28 14:34:01 +0900

    [add]タスクを表示するコンポーネントを新規作成

---
+++ app/javascript/pages/task/components/TaskItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="bg-white border shadow-sm rounded my-2 p-4">
+    <span>{{ task.title }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TaskItem",
+  props: {
+    task: {
+      title: {
+        type: String,
+        required: true
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
