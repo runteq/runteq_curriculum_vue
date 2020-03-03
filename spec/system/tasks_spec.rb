73db4de326bf2ec747582497d3843bd2fe6b5656
Makoto Otsuka <mametra2580@gmail.com>
2020-03-03 19:30:28 +0900

    [update]タスク詳細モーダルの表示と非表示のテストを追加

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -29,4 +29,24 @@ RSpec.describe 'タスク管理', type: :system do
     expect(page).to have_content('Rubyのサンプルコードを書く'), '作成したタスクがタスクページに表示されていません'
     expect(page).to have_content('Dockerを勉強する'), '作成したタスクがタスクページに表示されていません'
   end
+
+  it 'タスクをクリックして、タスク詳細モーダルが表示されている' do
+    task = create(:task, title: 'JavaScriptのfor文を理解する', description: 'JavaScript本格入門の第二章に書かれているfor文を読んで理解する')
+    visit '/tasks'
+    find("#task-#{task.id}").click
+    within "#task-modal-#{task.id}" do
+      expect(page).to have_content('JavaScriptのfor文を理解する'), 'タスク詳細モーダルにタイトルが表示されていません'
+      expect(page).to have_content('JavaScript本格入門の第二章に書かれているfor文を読んで理解する'), 'タスク詳細モーダルに説明文が表示されていません'
+    end
+  end
+
+  it 'モーダル表示状態で「閉じる」をクリックするとモーダルが非表示になる' do
+    task = create(:task, title: 'JavaScriptのfor文を理解する', description: 'JavaScript本格入門の第二章に書かれているfor文を読んで理解する')
+    visit '/tasks'
+    find("#task-#{task.id}").click
+    within "#task-modal-#{task.id}" do
+      click_button('閉じる')
+    end
+    expect(page).to have_no_button('閉じる'), 'タスク詳細モーダルが閉じられていません'
+  end
 end
