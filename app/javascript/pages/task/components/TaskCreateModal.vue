26ff36374a5f62ef54650e298388d0f7abf13c20
Makoto Otsuka <mametra2580@gmail.com>
2020-03-04 18:12:34 +0900

    [add]タスク追加モーダルコンポーネントの新規追加

---
+++ app/javascript/pages/task/components/TaskCreateModal.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="task-create-modal">
+    <div class="modal" @click.self="handleCloseModal">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-body">
+            <div class="form-group">
+              <label for="title">タイトル</label>
+              <input type="text" class="form-control" id="title" v-model="task.title">
+            </div>
+            <div class="form-group">
+              <label for="description">説明文</label>
+              <textarea class="form-control" id="description" rows="5" v-model="task.description"></textarea>
+            </div>
+            <button class="btn btn-success">追加</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="modal-backdrop show"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TaskCreateModal",
+  data() {
+    return {
+      task: {
+        title: '',
+        description: ''
+      }
+    }
+  },
+  methods: {
+    handleCloseModal() {
+      this.$emit('close-modal')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.modal {
+  display: block;
+}
+</style>
