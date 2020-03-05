bb51a741cf7e0ed484c0cac0c7b57104596eebe1
DaichiSaito <hrkedz@gmail.com>
2020-03-05 16:24:20 +0900

    [Add]Userモデルのemail属性に必須バリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
-  validates :email, uniqueness: true
+  validates :email, uniqueness: true, presence: true
   validates :name, presence: true
 end
