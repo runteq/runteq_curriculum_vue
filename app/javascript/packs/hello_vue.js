6e28eff27508b26d0687d7c3a170ae556f8ccadb
Makoto Otsuka <mametra2580@gmail.com>
2020-03-05 15:04:29 +0900

    [add]vueのプロダクションのヒントは不要なので非表示にする

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -8,6 +8,8 @@
 import Vue from 'vue'
 import App from '../app.vue'
 
+Vue.config.productionTip = false
+
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
     render: h => h(App)
