281c5bec9ac34bb40964d0001df4fc5c6f2dff98
Makoto Otsuka <mametra2580@gmail.com>
2020-03-13 14:01:15 +0900

    [clean]インスタンスオプションの並び順を整理

--- app/javascript/pages/task/components/TaskList.vue
+++ app/javascript/pages/task/components/TaskList.vue
@@ -3,18 +3,19 @@
     <div :id="taskListId" class="bg-light rounded shadow m-3 p-3">
       <slot name="header">タスク区分</slot>
       <template v-for="task in tasks">
-        <TaskItem :task="task"
-          @handleShowTaskDetailModal="$listeners['handleShowTaskDetailModal']"
-        >
-        </TaskItem>
+        <TaskItem :task="task" @handleShowTaskDetailModal="$listeners['handleShowTaskDetailModal']" />
       </template>
     </div>
   </div>
 </template>
 <script>
 import TaskItem from "./TaskItem"
+
 export default {
   name: "TaskList",
+  components: {
+    TaskItem
+  },
   props: {
     tasks: {
       type: Array,
@@ -24,9 +25,6 @@ export default {
       type: String,
       required: true
     }
-  },
-  components: {
-    TaskItem
   },
 }
 </script>
