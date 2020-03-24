f10a1efcee45c164b43fe6a499399ceb1a9d494e
Makoto Otsuka <mametra2580@gmail.com>
2020-03-24 13:26:59 +0900

    [update]Active Storageでアップロードしたファイルをフロントで表示させるために無理やりrouteを追加した

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,10 @@ Rails.application.routes.draw do
     resources :profile
   end
 
+  # TODO: Active Storageでアップロードしたファイルをフロントで表示させるときに使用している
+  # ただ、よりよい書き方があるはずなので要検討である
+  get '/rails/active_storage/blobs/:signed_id/:filename', to: 'active_storage/blobs#show'
+  get '/rails/active_storage/disk/:encoded_key/:filename', to: 'active_storage/disk#show'
+
   get '*path', to: 'home#index'
 end
