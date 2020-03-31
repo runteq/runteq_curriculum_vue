eca86f85ffe2f1cae0a104c0a5661d5e91ec9f42
DaichiSaito <hrkedz@gmail.com>
2020-03-31 20:48:08 +0900

    [add]capistranoの導入

---
+++ config/deploy.rb
@@ -0,0 +1,96 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.11.2"
+
+set :application, "runteq_curriculum_vue"
+set :repo_url, "git@github.com:runteq/runteq_curriculum_vue.git "
+set :user, 'runteq'
+# Default branch is :master
+set :branch, ENV['BRANCH'] || 'deploy'
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, "/var/www/runteq_curriculum_vue"
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+set :linked_files, %w[config/database.yml config/master.key db/runteq_curriculum_vue_production.sqlite3]
+
+# Default value for linked_dirs is []
+set :linked_dirs, %w[bin log tmp/pids tmp/cache tmp/sockets vendor/bundle node_modules public/uploads]
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for local_user is ENV['USER']
+# set :local_user, -> { `git config user.name`.chomp }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+# Uncomment the following to require manually verifying the host key before first deploy.
+# set :ssh_options, verify_host_key: :secure
+
+set :rbenv_type, :user
+set :rbenv_ruby, File.read('.ruby-version').strip
+set :rbenv_path, '/usr/local/rbenv'
+
+set :puma_threds, [4, 16]
+set :puma_workers, 0
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log, "#{shared_path}/log/puma.access.log"
+set :puma_preload_app, true
+
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
+  end
+
+  before :start, :make_dirs
+end
+
+namespace :deploy do
+  desc 'upload important files'
+  task :upload do
+    on roles(:app) do
+      sudo :mkdir, '-p', "#{shared_path}/config"
+      sudo %[chown -R #{fetch(:user)}.#{fetch(:user)} /var/www/#{fetch(:application)}]
+      sudo :mkdir, '-p', '/etc/nginx/sites-enabled'
+      sudo :mkdir, '-p', '/etc/nginx/sites-available'
+
+      upload!('config/database.yml', "#{shared_path}/config/database.yml")
+      upload!('config/master.key', "#{shared_path}/config/master.key")
+    end
+  end
+
+  desc 'Create database'
+  task :db_create do
+    on roles(:db) do
+      with rails_env: fetch(:rails_env) do
+        within release_path do
+          execute :bundle, :exec, :rake, 'db:create'
+        end
+      end
+    end
+  end
+
+  before :starting, :upload
+  before 'check:linked_files', 'puma:nginx_config'
+end
+
+after 'deploy:published', 'nginx:restart'
+before 'deploy:migrate', 'deploy:db_create'
---
+++ config/deploy/production.rb
@@ -0,0 +1,3 @@
+role :app, 'runteq@ec2-13-115-124-73.ap-northeast-1.compute.amazonaws.com'
+role :web, 'runteq@ec2-13-115-124-73.ap-northeast-1.compute.amazonaws.com'
+role :db, 'runteq@ec2-13-115-124-73.ap-northeast-1.compute.amazonaws.com'
